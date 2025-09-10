15ad618d3f6887fd9582573d96c30a7397ebd61c
Nandini Taru <nandinitaru57@gmail.com>
2025-09-10 14:03:50 +0530

    backend setup

---
+++ blogNest/blogNest/src/test/java/com/example/blogNest/BlogNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
